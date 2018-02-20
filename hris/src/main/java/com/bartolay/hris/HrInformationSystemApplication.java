29a08c61b8c218725a4c1b72cff73af73be73819
rbartolay <rbartolay@192.168.4.78>
2018-02-20 12:30:41 +0800

    initial files

---
+++ hris/src/main/java/com/bartolay/hris/HrInformationSystemApplication.java
@@ -0,0 +1,12 @@
+package com.bartolay.hris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrInformationSystemApplication.class, args);
+	}
+}
---
+++ hris/src/test/java/com/bartolay/hris/HrInformationSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bartolay.hris;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HrInformationSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
